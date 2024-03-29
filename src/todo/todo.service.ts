09f81a8260bf944bfe8fa026d4db00f440b3ca8e
Kostya Naida <varkac69@gmail.com>
2024-03-29 17:18:44 +0200

    test

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common'
 @Injectable()
 export class TodoService {
   getAll(): string[] {
-    return ['Test1', 'Test2', 'Test3', 'test4']
+    return ['Test1', 'Test2', 'Test3', 'test4', 'test5']
   }
 }
